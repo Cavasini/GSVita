e3351c17144b972e2b35e9cdd717b14accb797b2
Matheus Cavasini <89914174+Cavasini@users.noreply.github.com>
2023-11-19 12:03:33 -0300

    Update Login.jsx

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,15 +1,22 @@
-import Forms from "../components/Login/Forms"
+import Forms from "../components/Login/Forms";
 
-const Login = () =>{
- 
-    return(<>
+const Login = () => {
+  const getUser = sessionStorage.getItem("userData");
+  const getSenha = sessionStorage.getItem("senhaData");
+
+  return (
+    <>
+      {getUser && getSenha ? (
+        (window.location = "/")
+      ) : (
         <div className="Login">
-            <div>
-                <h2>Entre na sua conta</h2>
-            </div>
-            <Forms/>
+          <div>
+            <h2>Entre na sua conta</h2>
+          </div>
+          <Forms />
         </div>
+      )}
     </>
-    )
-}
-export default Login
+  );
+};
+export default Login;
